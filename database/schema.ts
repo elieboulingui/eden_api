04d65c2a23785f3039029c3a58c069eab63bfe32
unknown <elieboulingui2@gmail.com>
2026-04-21 23:12:16 +0100

    uidpate

--- database/schema.ts
+++ database/schema.ts
@@ -466,13 +466,7 @@ export class ProductSchema extends BaseModel {
   @column()
   declare imageUrl: string | null
   @column()
-  declare imageUrl: string | null
-  @column()
   declare isArchived: boolean
-  @column()
-  declare isNew: boolean | null
-  @column()
-  declare isOnSale: boolean | null
   @column()
   declare isNew: boolean | null
   @column()
@@ -777,10 +771,6 @@ export class UserSchema extends BaseModel {
   @column()
   declare facadePhoto2Url: string | null
   @column()
-  declare facadePhoto1Url: string | null
-  @column()
-  declare facadePhoto2Url: string | null
-  @column()
   declare facebookUrl: string | null
   @column()
   declare fullName: string | null
@@ -794,10 +784,6 @@ export class UserSchema extends BaseModel {
   declare idNumber: string | null
   @column()
   declare instagramUrl: string | null
-  @column()
-  declare interiorPhoto1Url: string | null
-  @column()
-  declare interiorPhoto2Url: string | null
   @column()
   declare interiorPhoto1Url: string | null
   @column()
@@ -830,14 +816,6 @@ export class UserSchema extends BaseModel {
   declare rccmDocumentUrl: string | null
   @column()
   declare rccmNumber: string | null
-  @column()
-  declare reference1Name: string | null
-  @column()
-  declare reference1Phone: string | null
-  @column()
-  declare reference2Name: string | null
-  @column()
-  declare reference2Phone: string | null
   @column()
   declare reference1Name: string | null
   @column()
