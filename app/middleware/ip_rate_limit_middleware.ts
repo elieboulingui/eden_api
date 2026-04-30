95a850f874a05f48998b95b363501a3989b391b2
elieboulingui <124277238+elieboulingui@users.noreply.github.com>
2026-04-30 16:10:12 +0100

    Update ip_rate_limit_middleware.ts

--- app/middleware/ip_rate_limit_middleware.ts
+++ app/middleware/ip_rate_limit_middleware.ts
@@ -3,8 +3,7 @@ import type { HttpContext } from '@adonisjs/core/http'
 import type { NextFn } from '@adonisjs/core/types/http'
 
 export default class IpRateLimitMiddleware {
-  async handle({ request, response }: HttpContext, next: NextFn) {
-    // ✅ Laisse tout passer sans limite
+  async handle(_ctx: HttpContext, next: NextFn) {
     return next()
   }
 }
