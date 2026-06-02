3ba0202044604185abb8ba6a80a9cfb17a72c78c
elieboulingui <124277238+elieboulingui@users.noreply.github.com>
2026-06-02 23:00:53 +0100

    Update CallbackController.ts

--- app/controllers/CallbackController.ts
+++ app/controllers/CallbackController.ts
@@ -104,7 +104,7 @@ export default class CallbackController {
                       quantity: item.quantity,
                       price: item.price,
                       subtotal: item.subtotal,
-                    })
+                    } as any)
                   }
                 }
               }
